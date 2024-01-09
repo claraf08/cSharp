a945ace1d24c768dc9289a25bee4d45ba1147a69
s88127 <s88127@bht-berlin.de>
2024-01-09 20:04:37 +0100

    upload

---
+++ src/Domain/Game.cs
@@ -0,0 +1,42 @@
+﻿namespace Domain;
+
+public class Game
+{
+    private Dice _dice1;
+    private Dice _dice2;
+    private readonly List<int> _highscores = new();
+
+    public int Eye1 => _dice1.Dots;
+    public int Eye2 => _dice2.Dots;
+    public bool HasSnakeEyes => Eye1 == 1 && Eye2 == 1;
+
+    public int Total { get; private set; }
+    public IReadOnlyList<int> HighScores => _highscores.AsReadOnly();
+
+    public Game()
+    {
+        Initialze();
+    }
+
+    public void Play()
+    {
+        _dice1.Roll();
+        _dice2.Roll();
+        if (HasSnakeEyes)
+        {
+            _highscores.Add(Total);
+            Total = 0;
+        }
+        Total += Eye1 + Eye2;
+    }
+
+    private void Initialze()
+    {
+        _dice1 = new Dice();
+        _dice2 = new Dice();
+    }
+    public void Restart()
+    {
+        Initialze();
+    }
+}
